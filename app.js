30de47a69d506ac9d04f67ece517338e45cd5ac9
Leonardo Oliveira <qwerty124563@gmail.com>
2020-06-04 21:29:25 +0100

    Add database.js conf, sequelizerc and use response formatter

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const { responseEnhancer } = require('express-response-formatter')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var malwaresRouter = require('./routes/malwares');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const malwaresRouter = require('./routes/malwares');
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(responseEnhancer());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
