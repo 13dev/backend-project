bb44d965ba80fcbb499cf84c5de186d5fc982acc
Leonardo Oliveira <qwerty124563@gmail.com>
2020-06-04 20:13:26 +0100

    Add malware model

--- models/index.js
+++ models/index.js
@@ -1,12 +1,12 @@
 const Sequelize = require('sequelize')
-const MalwareModel = require('malware')
+const MalwareModel = require('./malware')
 
 const sequelize = new Sequelize('database.db', null, null, {
     storage: './database-dev.sqlite3',
     dialect: 'sqlite',
 })
 
-const Malware = MalwareModel(Sequelize, sequelize);
+const Malware = MalwareModel(sequelize, Sequelize);
 
 module.exports = {
   Malware,
