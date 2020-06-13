69d1b8f42b2e12364ef8a787c7f8cbffb1710e10
Leonardo Oliveira <qwerty124563@gmail.com>
2020-06-13 15:46:09 +0100

    add auth controller

---
+++ controllers/AuthController.js
@@ -0,0 +1,9 @@
+module.exports = new class AuthController {
+    login (request, response, next) {
+        //TODO: to implement
+    }
+
+    register (request, response, next) {
+        //TODO: to implement
+    }
+};
