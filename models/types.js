6ad617720cdfd2c6fb66b320b6f0bd9c40e15979
Leonardo Oliveira <qwerty124563@gmail.com>
2020-06-14 17:08:57 +0100

    remove relations

--- models/types.js
+++ models/types.js
@@ -3,15 +3,18 @@ module.exports = (sequelize, DataTypes) => {
   const Types = sequelize.define(
     "Types",
     {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
       typeName: DataTypes.STRING,
     },
     {}
   );
   Types.associate = function (models) {
-    // Types.belongsTo(models.Malware, {
-    //   as: 'malware',
-    //   foreignKey: 'type_id',
-    // })
+    //Types.belongsTo(models.Malware)
   };
   return Types;
 };
