e73cac187ed34d693481bb248cb3845ae2ce5f61
Leonardo Oliveira <qwerty124563@gmail.com>
2020-06-14 17:31:26 +0100

    Add multer service

---
+++ services/multer.js
@@ -0,0 +1,13 @@
+const multer = require('multer')
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '/tmp/my-uploads')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+})
+
+module.exports = multer({ storage: storage })
